c9474bfcf4895b61ddc518019cd94589db2d8797
techbeast0 <nipun9759744800@gmail.com>
2025-02-02 11:59:38 +0530

    dvc pipeline succesfully executed

--- src/cnnClassifier/pipeline/stage_02_prepare_base_model.py
+++ src/cnnClassifier/pipeline/stage_02_prepare_base_model.py
@@ -1,9 +1,10 @@
-from src.cnnClassifier.config.configuration import ConfigurationManager
-from src.cnnClassifier.components.prepare_base_model import PrepareBaseModel
-from src.cnnClassifier import logger
+from cnnClassifier.config.configuration import ConfigurationManager
+from cnnClassifier.components.prepare_base_model import PrepareBaseModel
+from cnnClassifier import logger
 
 
 STAGE_NAME = "Prepare base model"
+
 
 class PrepareBaseModelTrainingPipeline:
     def __init__(self):
@@ -15,3 +16,16 @@ class PrepareBaseModelTrainingPipeline:
         prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
         prepare_base_model.get_base_model()
         prepare_base_model.update_base_model()
+
+
+    
+if __name__ == '__main__':
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
